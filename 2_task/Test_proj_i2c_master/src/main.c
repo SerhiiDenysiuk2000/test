4c60b3dd5ea9e9d49e8da1533a834b806d43390c
1SerhiiDenysiuk <34425938+1SerhiiDenysiuk@users.noreply.github.com>
2023-02-13 11:34:39 +0200

    added solution

---
+++ 2_task/Test_proj_i2c_master/src/main.c
@@ -0,0 +1,20 @@
+#include "driver/i2c.h"
+
+#define I2C_MASTER_SDA_IO 21
+#define I2C_MASTER_SCL_IO 22
+#define I2C_MASTER_FREQ_HZ 400000
+
+void app_main()
+{
+    int i2c_master_port = 1;
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+    };
+    i2c_param_config(i2c_master_port, &conf);
+    i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0);
+}
